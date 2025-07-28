87f4da516cd5caaf0f1213e10bf7483c17dc2e52
Nagendra Garapati <nagendragarapati09@gmail.com>
2025-07-28 12:07:46 +0530

    Add Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="main-container">
+
+
+
+
+      {/* <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained" className='btn'>Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+
+      <img src="https://res.cloudinary.com/dxww8lp4l/image/upload/v1753676577/20250727_181124_ccv6yt.jpg"/> */}
+
+
+
+    </div>
   )
 }
 
